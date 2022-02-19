137a0834abe43b808e0b8bbb1bd5904c74d6c4ac
Kexin-Li <hollalikexin@gmail.com>
2022-02-19 10:30:26 +0800

    update build command

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,7 @@ import { PNG } from "pngjs";
 let tray;
 
 function createTray() {
-  console.log(join(__dirname, "iconTemplate@2x.png"));
-  tray = new Tray(join(__dirname, "iconTemplate@2x.png"));
+  tray = new Tray(join(__dirname, "images/iconTemplate@2x.png"));
   const menu = Menu.buildFromTemplate([
     {
       label: "Screenshot",
