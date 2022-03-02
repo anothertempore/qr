c08aadc773d48fba87d36e9a981c58921f2c6068
Kexin-Li <hollalikexin@gmail.com>
2022-03-02 23:57:44 +0800

    update build steps

---
+++ scripts/notarize.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { notarize } = require("electron-notarize");
+
+exports.default = async function notarizing(context) {
+  const appName = context.packager.appInfo.productFilename;
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+
+  let appPath = `${appOutDir}/${appName}.app`;
+  let { appleId, appBundleId, appleIdPassword } = process.env;
+
+  return await notarize({
+    appBundleId,
+    appPath,
+    // ascProvider,
+    appleId,
+    appleIdPassword,
+  });
+};
